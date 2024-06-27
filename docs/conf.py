e82b2889ed22f25cefded6fd089e41faba53e3c3
Lean Sheng Tan <sheng.tan@9elements.com>
2024-06-27 15:17:58 +0200

    Update readme for build
    Add info on the supported build tools versions.
    
    Signed-off-by: Lean Sheng Tan <sheng.tan@9elements.com>

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ release = version
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '1.2.3'
+needs_sphinx = '7.3.7'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
